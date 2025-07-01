97fa95ffae507323764e90e11a57c39024852ab5
rahuljmanoj <117020393+rahuljmanoj@users.noreply.github.com>
2025-07-01 09:28:26 +0000

    Deploying to gh-pages from @ rahuljmanoj/rahuljmanoj.github.io@01977d2bc04ed6068c63919eed390ecda0f6d4c1 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -149,7 +149,7 @@ ninja.data = [{
           section: "News",},{id: "news-joined-as-a-postdoctoral-reserach-fellow-with-the-division-of-biomechanics-department-of-structral-engineering-at-ntnu-norway",
           title: 'Joined as a Postdoctoral Reserach Fellow with the Division of Biomechanics, Department of...',
           description: "",
-          section: "News",},{id: "news-featured-in-swayam-prabha-yt-channel-ch-32-iit-madras-bridge-courses-impact-series-on-explaining-the-cardiovascular-initiatives-at-htic-iit-madras",
+          section: "News",},{id: "news-featured-in-swayam-prabha-yt-channel-ch-32-iit-madras-bridge-courses-impact-series-on-discussing-the-cardiovascular-initiatives-at-htic-iit-madras",
           title: 'Featured in Swayam Prabha YT Channel CH 32 IIT Madras Bridge Courses, Impact...',
           description: "",
           section: "News",handler: () => {
